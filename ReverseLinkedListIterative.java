d258d54b579236857c7ec6323fade6c87c714fe2
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-08-17 22:09:37 -0400

    Create ReverseLinkedListIterative.java

---
+++ ReverseLinkedListIterative.java
@@ -0,0 +1,51 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedListIterative {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = null;
+
+        while (current != null) {
+            next = current.next;   // save next node
+            current.next = prev;   // reverse current node's link
+            prev = current;        // move prev forward
+            current = next;        // move current forward
+        }
+
+        return prev; // new head
+    }
+
+    // Helper function to print the list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        // Create linked list: 1 -> 2 -> 3 -> 4 -> null
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.println("Reversed list:");
+        printList(head);
+    }
+}
